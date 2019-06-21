894d069c64f327e5681ce7f451d04450d7e91018
xiangxingx <xiangxingx@gmail.com>
2019-06-21 17:44:19 +0800

    整理代码

--- js/message.js
+++ js/message.js
@@ -1,22 +1,78 @@
-var APP_ID = 'U8OXtVsSq93S09f9BtvAsx74-gzGzoHsz';
-var APP_KEY = '8TIYFxc5x9bu88nB7Bp4vGT7';
+// leanCloud
+!function () {
+  var view = document.querySelector('section.message')
+  var model = {
+    init: function () {
+      var APP_ID = 'O3SKGkzld4qyFCd7qOpsTp7A-9Nh9j0Va'
+      var APP_KEY = 'W4uq0d3kcB7uHpwLdL7duIVK'
+      AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+      })
+    },
+    // 获取数据
+    fetch: function () {
+      var query = new AV.Query('message')
+      return query.find() // promise对象
+    },
+    // 创建数据
+    save: function (name, content) {
+      var Message = AV.Object.extend('message')
+      var message = new Message()
+      return message.save({ // promise对象
+        name: name,
+        content: content
+      })
+    }
+  }
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+  var controller = {
+    view: null,
+    messageList: null,
+    model: null,
+    init: function (view) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('#messageList')
+      this.form = document.querySelector('#postMessageForm')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
 
-let myForm = document.querySelector('#postMessageForm')
-myForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-  let content = myForm.querySelector('input[name = content]').value
-  console.log(content)
-  var Message = AV.Object.extend('message');
-  var message = new Message();
-  message.save({
-    content: content
-  }).then(function (object) {
-    alert('上传成功');
-  })
-})
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        }
+      )
+    },
+    bindEvents: function () { // 只管绑定事件
+      this.form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function () {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name = content]').value
+      let name = myForm.querySelector('input[name = name]').value
+      this.model.save(name, content).then(
+        function (object) {
+          let li = document.createElement('li')
+          li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+          let messageList = document.querySelector('#messageList')
+          messageList.appendChild(li)
+          myForm.querySelector('input[name = content]').value = ''
+        }
+      )
+    }
+  }
 
+  controller.init(view, model)
+}.call()
